3305b43d692c06c0becd85639212a673058cc332
waderice52224 <waderice52224@gmail.com>
2024-03-12 15:27:28 -0600

    Added boilerplate functions to logic_test.py

--- tests/logic_test.py
+++ tests/logic_test.py
@@ -1,1 +1,31 @@
 from logic import *
+def test_addNonZeroValuesToChoices():
+    return "TODO"
+
+
+def test_replaceZeros():
+    return "TODO"
+
+
+def test_replaceZeros():
+    return "TODO"
+
+
+def test_fillChoices():
+    return "TODO"
+
+
+def test_fillChoicesTable():
+    return "TODO"
+
+
+def test_addCorrectValuesToBoard():
+    return "TODO"
+
+
+def test_requiredValue():
+    return "TODO"
+
+
+def test_notDone():
+    return "TODO"
